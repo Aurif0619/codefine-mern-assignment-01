320d54a9d4a0395d5ba2142dd8399cfd1f827c63
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-01-23 23:16:15 +0500

    Data fetch successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
+import Products from './components/product/Products';
 
 function App() {
   return (
-   <>
-   <Navbar/>
-   </>
+    <>
+      <Navbar />
+      <Products />
+    </>
   );
 }
 
